50eb548954da45222c08e13d1855d83a28dcb52d
Musharraf188 <mr.musharraf188@gmail.com>
2023-07-23 17:08:40 +0530

    useEffecet

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
@@ -10,13 +10,23 @@ const Login = (props) => {
   const [enteredPassword, setEnteredPassword] = useState('');
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
-
+useEffect(()=>{
+  const identifier= setTimeout(()=>{
+    setFormIsValid(
+      console.log('Checking From Validation'),
+      enteredEmail.includes('@') && enteredPassword.trim().length > 6
+    );
+  },1000)
+  return ()=>{
+    console.log('CLEANUP');
+    clearTimeout(identifier);
+  }
+ 
+}, [enteredEmail, enteredPassword]);
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
 
-    setFormIsValid(
-      event.target.value.includes('@') && enteredPassword.trim().length > 6
-    );
+   
   };
 
   const passwordChangeHandler = (event) => {
